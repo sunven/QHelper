09f12c1a88511f01d4dfbe089cc8fe6c2101ab05
sunven <604164227@qq.com>
2021-12-16 00:22:44 +0800

    add openinvscode

---
+++ content/openinvscode.js
@@ -0,0 +1,18 @@
+async function insertVSCodeButton() {
+  // .btn.d-none.d-md-block 取go to file按钮
+  // span.d-none.d-md-flex 去code按钮
+  let insertNode = document.querySelector('.btn.d-none.d-md-block') || document.querySelector('span.d-none.d-md-flex');
+  if (!insertNode) {
+    return;
+  }
+  const arr = window.location.pathname.split('/');
+  if (arr.length < 3) {
+    return;
+  }
+  const target = `https://vscode.dev/github/${arr[1]}/${arr[2]}`;
+  const layoutClass = insertNode.classList.contains('ml-2') ? 'ml-2' : 'mr-2';
+  const btn = `<a style="background-color: rgb(45, 164, 78);color: white;" class="btn ${layoutClass} d-none d-md-block" target="_blank" href="${target}"> vscode.dev </a>`;
+  insertNode.insertAdjacentHTML('beforebegin', btn);
+}
+
+insertVSCodeButton();
